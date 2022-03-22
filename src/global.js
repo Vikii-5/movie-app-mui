e7e001b7dd8216ac962b6116b43120b97e9bde89
Vikii-5 <vikkycsk3@gmail.com>
2022-03-22 10:51:38 +0530

    api changed

--- src/global.js
+++ src/global.js
@@ -1,1 +1,2 @@
-export const API = "https://6197229daf46280017e7e453.mockapi.io"
+// export const API = "https://6197229daf46280017e7e453.mockapi.io"
+export const API = "https://vikii-node-app.herokuapp.com"
